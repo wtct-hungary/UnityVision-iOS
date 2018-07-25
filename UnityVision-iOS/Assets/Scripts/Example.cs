8131e6709c7fa603854a07078b4bce1b9ac37818
Adam Hegedus <Adam.Hegedus@POSSIBLE.com>
2018-07-25 12:11:29 +0200

    Fixed comments.

--- UnityVision-iOS/Assets/Scripts/Example.cs
+++ UnityVision-iOS/Assets/Scripts/Example.cs
@@ -1,11 +1,10 @@
 ﻿using System.Linq;
-using System.Runtime.CompilerServices;
 using Possible.Vision;
 using UnityEngine;
 using UnityEngine.UI;
 
 /// <summary>
-/// This example shows how to perform real-time image classification using the video feed from the rear camera. 
+/// This example shows how to perform real-time image classification, using the video feed from the rear camera. 
 /// </summary>
 public class Example : MonoBehaviour
 {
@@ -26,11 +25,10 @@ public class Example : MonoBehaviour
 		
 		// We need to tell the Vision plugin what kind of requests do we want it to perform.
 		// This call not only prepares the managed wrapper for the specified image requests,
-		// but allocates VisionRequest objects on the native side. You only need to call this
-		// method when you initialize your application, and later if you need to change the type
-		// of request you want to perform.
-		// When performing image image classification requests,
-		// maxObservations refers to the number of guesses extracted, ordered by confidence.
+		// but allocates VNRequest objects on the native side. You only need to call this
+		// method when you initialize your app, and later if you need to change the type
+		// of requests you want to perform. When performing image classification requests,
+		// maxObservations refers to the number of returned guesses, ordered by confidence.
 		_vision.SetAndAllocateRequests(VisionRequest.Classification, maxObservations: 1);
 	}
 
@@ -46,7 +44,7 @@ public class Example : MonoBehaviour
 
 	private void Start()
 	{
-		// Start capturing
+		// Start capturing images from device camera
 		_webCamTexture.Play();
 	}
 
@@ -58,13 +56,15 @@ public class Example : MonoBehaviour
 		{
 			// This is the call where we pass in the handle to the image data to be analysed
 			_vision.EvaluateBuffer(
+	
 				// This argument is always of type IntPtr, that refers to the data buffer
 				buffer: _webCamTexture.GetNativeTexturePtr(), 
-				// We need to tell the plugin the nature of the underlying data.
+	
+				// We need to tell the plugin about the nature of the underlying data.
 				// The plugin only supports CVPixelBuffer (CoreVideo) and MTLTexture (Metal).
 				// Unity's Texture and all of its derived types return MTLTextureRef
-				// when using Metal graphics API on iOS. OpenGLES 2 is not supported.
-				// For more information refer to the official API documentation:
+				// when using Metal graphics API on iOS. OpenGLES 2 is not supported
+				// by the plugin. For more information refer to the official API documentation:
 				// https://docs.unity3d.com/ScriptReference/Texture.GetNativeTexturePtr.html
 				dataType: ImageDataType.MetalTexture);
 		}
